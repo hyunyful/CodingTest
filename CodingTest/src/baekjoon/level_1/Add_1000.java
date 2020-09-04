8a51d451745eb1e21c73a33155d4ecb15e8a3a38
DESKTOP-NBC7N82\ffiwe <ffiwe@naver.com>
2020-09-05 03:54:58 +0900

    백준 1000 A+B

--- CodingTest/src/baekjoon/level_1/Add_1000.java
+++ CodingTest/src/baekjoon/level_1/Add_1000.java
@@ -6,13 +6,19 @@ public class Add_1000 {
 
 	public static void main(String[] args) {
 		
+		/*
+		두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		
+		 1 2 -> 3
+		*/
+		
 		Scanner scan = new Scanner(System.in);
 		
 		String input = scan.nextLine();
 		int A = Integer.parseInt(input.split(" ")[0]);
 		int B = Integer.parseInt(input.split(" ")[1]);
 		
-		System.out.print(A-B);
+		System.out.print(A+B);
 
 	}
 
